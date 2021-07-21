61215e9623de8eb6588ea901a8b2289752f18315
Dan Davison <dandavison7@gmail.com>
2021-07-21 23:42:22 +0100

    Bump grpcio and grpclib versions

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setuptools.setup(
     install_requires=[
         "betterproto-for-temporal-python-sdk==1.2.5",
         "dataclasses-json==0.3.8",
-        "grpcio==1.30.0",
-        "grpclib==0.3.2",
+        "grpcio==1.38.1",
+        "grpclib==0.4.1",
         "h2==3.2.0",
         "more-itertools==7.0.0",
         "pytz==2020.1",
